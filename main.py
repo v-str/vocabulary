11f8ee1f0b17ab62754459e039a00f79593c8b4c
v-str <w.strepetov@gmail.com>
2021-05-06 13:29:55 +0300

    Print banner head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-import misc
+import banner
 
-misc.getWord()
+banner.showBanner()
--- misc.py
+++ misc.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-def ShowBanner():
-    for i in range(0, 9):
-        print("*")
-
 
 def getWord():
     word = input("word: ")
