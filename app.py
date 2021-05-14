fc4305534c68ca1dd451718f6bacec8fa1d5a91f
v-str <w.strepetov@gmail.com>
2021-05-14 12:59:21 +0300

    Add basic functions of application to main loop

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import sys
 from core import msg
 from core import cfg
 from core import input_processor as ipp
-from core import vocabulary
-from core import file_worker
+from core import vocabulary as voc
+from core import file_worker as fw
 
 
 def main():
@@ -20,6 +20,12 @@ def runMainLoop():
         user_input = ipp.getUserInputString(cfg.enter_sym)
         if ipp.isUserWantToQuit(user_input):
             sys.exit()
+        if ipp.isUserWantToAdd(user_input):
+            voc.tryToAdd()
+        if ipp.isUserWantToShow(user_input):
+            voc.showContext()
+        if ipp.isUserWantToViewHelp(user_input):
+            msg.showHelp()
 
 
 main()
