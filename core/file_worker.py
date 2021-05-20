185e0d9d37eb1d75702af5cdee6cbc491d547b72
v-str <w.strepetov@gmail.com>
2021-05-20 22:52:14 +0300

    Add load function

--- core/file_worker.py
+++ core/file_worker.py
@@ -36,12 +36,18 @@ def writeToFile():
 
 def loadFromFile():
     temp_list, strings_len = __readFile__()
+    temp_dict = dict()
     if len(temp_list) == 0:
         print("Nothing to load")
     else:
         for line in temp_list:
-            newline = re.sub(r"\d+\) ", '', line)
-            print(newline)
+            stripline = line.strip()
+            trline = re.sub(r"\d+\) ", '', stripline)
+            newstr = trline.split(" - ")
+            temp_dict[newstr[0]] = newstr[1]
+
+    cfg.app_dict.update(temp_dict)
+    print("Vocabulary loaded")
 
 
 def __isNothingToWrite__():
@@ -71,4 +77,4 @@ def __readFile__():
     except FileNotFoundError as err:
         print("file not exist, nothing to read")
 
-    return 1
+    return list(), 0
